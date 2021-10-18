736797041a485caf1e2b73992d933caaa73cab55
anedergaard <ape.anp@gmail.com>
2021-10-18 12:13:01 +0200

    discover script and connect example

---
+++ discoverBeoremoteHalo.py
@@ -0,0 +1,26 @@
+from zeroconf import ServiceBrowser, Zeroconf
+
+
+class BeoremoteHaloListener:
+
+    def remove_service(self, zeroconf, type, name):
+        return
+
+    def add_service(self, zeroconf, type, name):
+        info = zeroconf.get_service_info(type, name)
+        print("%s" % info.server[:-1])
+
+    def update_service(self, zeroconf, type, name):
+        return
+
+
+if __name__ == "__main__":
+    zeroconf = Zeroconf()
+    listener = BeoremoteHaloListener()
+    browser = ServiceBrowser(zeroconf, "_zenith._tcp.local.", listener)
+    try:
+        input(
+            "Discovering Beoremote Halo on network...\r\nIf Beoremote Halo does not appear on the list, try waking it "
+            "up while discovering in running.\r\n\r\nPress enter to exit...\n\n")
+    finally:
+        zeroconf.close()
---
+++ examples/README.md
@@ -0,0 +1,6 @@
+# Exmaples
+This directory contains three examples
+
+1. Connect and Listen
+2. Connect and Configure
+3. Connect and Interact
