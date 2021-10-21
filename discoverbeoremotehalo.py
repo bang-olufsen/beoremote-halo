5847d0a229bc6b25fb5521f220b254eb55e657a0
anedergaard <ape.anp@gmail.com>
2021-10-21 12:21:52 +0200

    catching keyboardinterrrupt

--- discoverbeoremotehalo.py
+++ discoverbeoremotehalo.py
@@ -76,6 +76,8 @@ if __name__ == "__main__":
             "Discovering Beoremote Halo on network...\r\n"
             "If Beoremote Halo does not appear on the list, try waking it "
             "up while discovering in running.\r\nPress enter to exit...\n\n")
+    except KeyboardInterrupt:
+        pass
     finally:
         zeroconf.close()
     sys.exit()
