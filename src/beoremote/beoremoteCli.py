46323fe5cef8365b05f8d9c62030879a634f9b64
anedergaard <ape.anp@gmail.com>
2021-11-03 13:47:38 +0100

    added listen+demo commands to cli bumped to 0.0.2

--- src/beoremote/beoremoteCli.py
+++ src/beoremote/beoremoteCli.py
@@ -1,7 +1,6 @@
 import click
-from beoremotehalo import BeoRemoteHalo
 
-from beoremote import discover
+from beoremote import backend_demo, beoremotehalo, discover
 
 cli = click.Group()
 
@@ -9,19 +8,20 @@ cli = click.Group()
 @cli.command(help="Scan network for active Beoremote Halo.")
 def scan():
     discover.discover()
+    pass
 
 
 @cli.command(help="Interactive Home Automation Demo")
 @click.option("--hostname", required=True)
 def demo(hostname):
-    print(hostname)
+    backend_demo.backend(hostname)
     pass
 
 
 @cli.command(help="Connect to Halo and listen for events")
 @click.option("--hostname", required=True)
 def listen(hostname):
-    remote = BeoRemoteHalo(hostname)
+    remote = beoremotehalo.BeoRemoteHalo(hostname)
     remote.connect()
     pass
 
