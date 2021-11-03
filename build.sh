53a214a98a1d4c6cfffbc11070fda402e4d82cd2
anedergaard <ape.anp@gmail.com>
2021-11-03 15:20:17 +0100

    helper scripts

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+case $1 in
+  --doc)
+    docker run --rm -it \
+    -v ${PWD}/beoremote-halo-spec.yaml:/app/asyncapi.yml \
+    -v ${PWD}/output:/app/output \
+    asyncapi/generator -o output /app/asyncapi.yml --force-write -p singleFile=true @asyncapi/html-template
 
-docker run --rm -it \
--v ${PWD}/beoremote-halo-spec.yaml:/app/asyncapi.yml \
--v ${PWD}/output:/app/output \
-asyncapi/generator -o output /app/asyncapi.yml --force-write -p singleFile=true @asyncapi/html-template
+    cp output/index.html index.html
+    ;;
 
-cp output/index.html index.html
+  --pip-package)
+    python3 -m build
+    ;;
+
+  --test)
+    coverage run --source=./src,./test -m pytest && coverage report
+    ;;
+esac
